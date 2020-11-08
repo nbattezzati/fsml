5555e1fd4bcc8b617acd65f83b1e6ed311cfa62c
Niccolo Battezzati <nik@MacBook-Air-di-Niccolo.local>
2020-11-08 18:40:42 +0100

    add example toy_decoder

---
+++ examples/toy_decoder/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "toy_decoder_fsm.h"
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    /* reset fsm to the starting state */
+    toy_decoder_fsm->reset();
+
+    /* execute FSM to search for the TOY pattern in the input string */
+    while(argv[1] != NULL && argv[1][i] != '\0') {
+        toy_decoder_fsm->set_input_char(argv[1][i]);
+        if (toy_decoder_fsm->exec() == toy_decoderState__toy_found) {
+            printf("TOY found\n");
+            return 0;
+        }
+        i++;
+    }
+
+    printf("TOY not found\n");
+    return 1;
+}
